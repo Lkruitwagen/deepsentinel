933fb0377b153d2a8724664b8d53112f4fee1577
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2020-08-20 08:57:31 +0100

    utils

---
+++ deepsentinel/utils/geoutils.py
@@ -0,0 +1,20 @@
+
+
+def get_utm_zone(lat,lon):
+    """A function to grab the UTM zone number for any lat/lon location
+    """
+    zone_str = str(int((lon + 180)/6) + 1)
+
+    if ((lat>=56.) & (lat<64.) & (lon >=3.) & (lon <12.)):
+        zone_str = '32'
+    elif ((lat >= 72.) & (lat <84.)):
+        if ((lon >=0.) & (lon<9.)):
+            zone_str = '31'
+        elif ((lon >=9.) & (lon<21.)):
+            zone_str = '33'
+        elif ((lon >=21.) & (lon<33.)):
+            zone_str = '35'
+        elif ((lon >=33.) & (lon<42.)):
+            zone_str = '37'
+
+    return zone_str
