c8af195ddd8746e2922b00b75c5029cd56a7b6d8
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2020-09-30 14:43:57 +0100

    update readme and async catalog

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# deepsentinel
-DeepSentinel: a sentinel-1 and -2 self-supervised sensor fusion model for general purpose semantic embedding 
+# _DeepSentinel_
+_DeepSentinel_: a sentinel-1 and -2 sensor fusion model for semantic embedding. A Copernicus Master's Finalist supported by Descartes Labs Impact Science Programme and Microsoft AI for Earth.
+
+## Motivation
+Earth observation offers new insight into anthropogenic changes to nature, and how these changes are effecting (and are effected by) the built environment and the real economy. With the global availability of medium-resolution (\~10m) synthetic aperature radar (SAR) Sentinel-1 and multispectral Sentinel-2 imagery, machine learning can be employed to offer these insights at scale, unbiased to company- and country-level reporting.
+
+Machine learning applications using earth observation imagery present some unique problems. Multispectral images are often confounded by interference from clouds and atmosphere conditions. And while there is ample imagery data, geospatially-localised labels are sparse, with data quality and completeness heavily geographically skewed.
+
+_DeepSentinel_ seeks to address these problems.
+
+
+
+DeepSentinel addresses a perential problem in machine learning with earth observation: the lack of training labels despite ample 
+
+Generated samples are being made available via two cloud storage 
+
+
+## Installation
+
+
+## Use
--- deepsentinel/utils/download_catalog.py
+++ deepsentinel/utils/download_catalog.py
@@ -5,21 +5,35 @@ from datetime import date, timedelta
 
 import multiprocessing as mp
 
+def async_product_caller(arg_tuples, N_workers):
+    
+    with mp.Pool(N_workers) as p:
+        p.starmap(async_product_worker, arg_tuples)
 
-def sentinel_products_mpcaller(start_date, period, N_periods, auth, N_workers):
+def async_product_worker(platform, product, date, auth):
+    api = SentinelAPI(auth['U'], auth['P'])
+    
+    products = api.query(date=(date, date+timedelta(days=1)),
+                            platformname=platform,
+                            producttype=product)
+
+    df = pd.DataFrame.from_dict(products, orient='index')
+
+    df.to_parquet(os.path.join(os.getcwd(),'data','catalog','_'.join([platform, product, date.isoformat()[0:10]])+'.parquet'))
+
+def period_mpcaller(start_date, period, N_periods, auth, N_workers):
 
     args = [(start_date, period, ii_t, auth['scihub']) for ii_t in range(N_periods)]
 
     with mp.Pool(N_workers) as p:
         p.starmap(sentinel_products_worker, args)
     
-def sentinel_products_worker(start_date, period, ii_t, auth):
+def period_worker(start_date, period, ii_t, auth):
     api = SentinelAPI(auth['U'], auth['P'])
     
     min_date = start_date + timedelta(days=period*ii_t)
     max_date = start_date + timedelta(days=period*(ii_t+1))
     
-    """
     S2_L2A_products = api.query(date=(min_date, max_date),
                             platformname='Sentinel-2',
                             producttype='S2MSI2A')
@@ -27,7 +41,6 @@ def sentinel_products_worker(start_date, period, ii_t, auth):
     S2_L2A_df = pd.DataFrame.from_dict(S2_products, orient='index')
 
     S2_L2A_df.to_parquet(os.path.join(os.getcwd(),'data','catalog',f'S2_L2A_{str(ii_t)}_{min_date.isoformat()[0:10]}_{max_date.isoformat()[0:10]}.parquet'))
-    """
     
     S2_L1C_products = api.query(date=(min_date, max_date),
                             platformname='Sentinel-2',
@@ -37,7 +50,6 @@ def sentinel_products_worker(start_date, period, ii_t, auth):
 
     S2_L1C_df.to_parquet(os.path.join(os.getcwd(),'data','catalog',f'S2_L1C_{str(ii_t)}_{min_date.isoformat()[0:10]}_{max_date.isoformat()[0:10]}.parquet'))
                             
-    """
     S1_products = api.query(date=(min_date, max_date),
                             platformname='Sentinel-1',
                             producttype='GRD')            
@@ -45,7 +57,6 @@ def sentinel_products_worker(start_date, period, ii_t, auth):
     S1_df = pd.DataFrame.from_dict(S1_products, orient='index')
     
     S1_df.to_parquet(os.path.join(os.getcwd(),'data','catalog',f'S1_{str(ii_t)}_{min_date.isoformat()[0:10]}_{max_date.isoformat()[0:10]}.parquet'))
-    """
 
 if __name__=="__main__":
     start_date = date(2019,8,1)
